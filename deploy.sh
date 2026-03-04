4fb7d02613c4f0d03f2957d0cced8bd0701a0658
Maxi <sergeev.maxs@gmail.com>
2026-03-04 14:34:54 -0300

    deploy.sh: add --clear-base-image flag for dockerfile builds

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ gcloud run deploy $SERVICE \
   --timeout 60 \
   --set-env-vars "OPENAI_API_KEY=${OPENAI_API_KEY:-}" \
   --min-instances 0 \
-  --max-instances 3
+  --max-instances 3 \
+  --clear-base-image
 
 echo "Deployed! URL: https://$SERVICE-473141067823.$REGION.run.app"
